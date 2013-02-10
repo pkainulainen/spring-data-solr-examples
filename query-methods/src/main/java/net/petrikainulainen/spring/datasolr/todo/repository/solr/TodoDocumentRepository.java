912ceccdb7eb2e64081c9dd133ceed5fae43d929
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-02-10 16:23:31 +0200

    Added Solr repository

---
+++ query-methods/src/main/java/net/petrikainulainen/spring/datasolr/todo/repository/solr/TodoDocumentRepository.java
@@ -0,0 +1,10 @@
+package net.petrikainulainen.spring.datasolr.todo.repository.solr;
+
+import net.petrikainulainen.spring.datasolr.todo.document.TodoDocument;
+import org.springframework.data.solr.repository.SolrCrudRepository;
+
+/**
+ * @author Petri Kainulainen
+ */
+public interface TodoDocumentRepository extends SolrCrudRepository<TodoDocument, Long> {
+}
