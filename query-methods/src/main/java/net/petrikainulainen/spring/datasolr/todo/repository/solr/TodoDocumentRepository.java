937e0b0e54b9ac895bbf5c2aa6ad5c4ef1ad407b
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-03-02 20:42:52 +0200

    Added query methods to repository

--- query-methods/src/main/java/net/petrikainulainen/spring/datasolr/todo/repository/solr/TodoDocumentRepository.java
+++ query-methods/src/main/java/net/petrikainulainen/spring/datasolr/todo/repository/solr/TodoDocumentRepository.java
@@ -1,10 +1,21 @@
 package net.petrikainulainen.spring.datasolr.todo.repository.solr;
 
 import net.petrikainulainen.spring.datasolr.todo.document.TodoDocument;
+import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
+
+import java.util.List;
 
 /**
  * @author Petri Kainulainen
  */
 public interface TodoDocumentRepository extends SolrCrudRepository<TodoDocument, String> {
+
+    public List<TodoDocument> findByTitleContainsOrDescriptionContains(String title, String description);
+
+    @Query(name = "TodoDocument.findByNamedQuery")
+    public List<TodoDocument> findByNamedQuery(String searchTerm);
+
+    @Query("title:*?0* OR description:*?0*")
+    public List<TodoDocument> findByQueryAnnotation(String searchTerm);
 }
